3cb46114c017403c378c94596af4d2b3e55ab554
Nick Haskins <nphaskins@gmail.com>
2018-12-15 14:15:30 -0500

    redirect unauth’d for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :warning, :danger, :info
   set_current_tenant_by_subdomain(:brand, :subdomain)
+
+  before_action :redirect_public
+
+  private
+
+    def redirect_public
+      unless ['index','show'].include?(action_name)
+        unless user_signed_in?
+          redirect_to brand_root_path
+        end
+      end
+    end
 end
