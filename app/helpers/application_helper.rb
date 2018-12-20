9852a32f7a3ce453eca9810370034453352e4165
Nick Haskins <nphaskins@gmail.com>
2018-12-20 15:18:04 -0500

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
     user_signed_in? &&
     current_tenant &&
     current_tenant.account.users.include?(current_user) ||
-    (current_user.account.brands.exists? && current_user.account.brands.include?(current_tenant) )
-
+    (current_user && current_user.account.brands.exists? && current_user.account.brands.include?(current_tenant) )
   end
 
   def date_display(date, time = true)
